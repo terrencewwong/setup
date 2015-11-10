39449142ad651f161bf7a6a08cd9fb9fd159ce41
Terrence Wong <wongterrencew@Terrences-Air.home>
2015-11-09 20:09:28 -0500

    Adding basic setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install git
+brew install wget
+brew install node
+brew install nodenv
+
+# Apps
+brew install caskroom/cask/brew-cask
+brew cask install google-chrome
+brew cask install firefox
+brew cask install spotify
