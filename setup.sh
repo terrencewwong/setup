d479ac2f9181a17fb1474758e52d9ad525a64eae
Terrence Wong <terrencewwong@MacBook-Air.local>
2017-11-20 22:40:26 +0100

    replace iterm for hyper

--- setup.sh
+++ setup.sh
@@ -3,19 +3,20 @@
 # Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew update
 brew install git
 brew install wget
 brew install node
 brew install nodenv
 brew install hub
-brew install ghi
 brew install tmux
 brew install docker
 brew install docker-compose
 
 # Apps
 brew install caskroom/cask/brew-cask
+brew cask update
 brew cask install google-chrome
 brew cask install firefox
 brew cask install spotify
-brew cask install iterm2
+brew cask install hyper
